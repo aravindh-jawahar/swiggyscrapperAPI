99e302a21f361cff1997c0d256b40e6a3b2e7b5c
Aravindh Jawahar <j.aravindh@zohomail.com>
2022-08-11 13:24:33 +0530

    Initialised the backend database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_075240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "daily_prices", force: :cascade do |t|
+    t.float "amount"
+    t.bigint "items_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["items_id"], name: "index_daily_prices_on_items_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "item_id"
+    t.bigint "menus_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menus_id"], name: "index_items_on_menus_id"
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.integer "item_id"
+    t.bigint "restaurants_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurants_id"], name: "index_menus_on_restaurants_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "areaPostalCode"
+    t.string "locality"
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+  end
+
+  add_foreign_key "daily_prices", "items", column: "items_id"
+  add_foreign_key "items", "menus", column: "menus_id"
+  add_foreign_key "menus", "restaurants", column: "restaurants_id"
 end
